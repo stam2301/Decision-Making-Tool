7b6978c4e849d6c11d3bfd46e3ddae8eb6969ae2
stam2301 <74074256+stam2301@users.noreply.github.com>
2020-11-09 19:04:17 +0200

    Model Method creation and migrations

--- thesisProject/mainApp/models.py
+++ thesisProject/mainApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Method (models.Model):
+    methodID = models.AutoField(primary_key=True)
+    input_file = models.JSONField()
+    output_file = models.JSONField()
+    date = models.DateTimeField(auto_now_add=True)
