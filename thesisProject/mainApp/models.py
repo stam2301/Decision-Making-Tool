1742bef88dde16441ed682479f618405e815d4d3
stam2301 <74074256+stam2301@users.noreply.github.com>
2020-11-29 03:44:40 +0200

    Fixed upload to directory

--- thesisProject/mainApp/models.py
+++ thesisProject/mainApp/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Method (models.Model):
     methodID = models.AutoField(primary_key=True)
     title = models.CharField(blank=True, max_length=50)
-    input_file = models.FileField(upload_to= 'inputs')
-    output_file = models.FileField( upload_to= 'outputs')
+    input_file = models.FileField(upload_to= 'static/inputs')
+    output_file = models.FileField( upload_to= 'static/outputs')
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
