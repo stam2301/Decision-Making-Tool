784f2e086d8dc60db8dc2ee991e9e686e71a043a
stam2301 <74074256+stam2301@users.noreply.github.com>
2020-12-03 19:48:07 +0200

    minor changes

--- thesisProject/dectree/utilities/dectree_algo.py
+++ thesisProject/dectree/utilities/dectree_algo.py
@@ -1,17 +1,8 @@
 import json, logging, os
-from django.templatetags.static import static
 
-input_dir = 'inputs/'
 
-def read_json (file_dir):
-    try:
-        print('Reading from input')
-        with open(static(file_dir), 'r') as f:
-            return json.load(f)
-    finally:
-        print('Done reading')
-
-def dectree_algo_main (input_file_name):
-    input_file_dir = os.path.join(input_dir,input_file_name)
-    return_dict = read_json(input_file_dir)
-    print(return_dict)
+def dectree_algo_main (input_file):
+    
+    data = json.load(input_file)
+    print("Done reading")
+    print(data)
